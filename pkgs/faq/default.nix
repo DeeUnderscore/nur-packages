fb6f183ce350580218b0b90e70af3d204f639f58
D Anzorge <d.anzorge@gmail.com>
2021-04-27 19:52:18 +0200

    faq: 0.0.6 → 0.0.7

--- pkgs/faq/default.nix
+++ pkgs/faq/default.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchFromGitHub, buildGoPackage, jq }:
 buildGoPackage rec {
   pname = "faq";
-  version = "0.0.6";
+  version = "0.0.7";
 
   src = fetchFromGitHub {
     owner = "jzelinskie";
     repo = "faq";
     rev = version;
-    sha256 = "0qrkmx7l008ya116v4d43x37psgkkvl53azyzj7hcg0g3qyz9ps4";
+    sha256 = "sha256:1766cpxcdjq1qj1iihp3s6y24czgw6lmdyrryzk3rhfbspcimcgh";
   };
   goDeps = ./deps.nix;
   goPackagePath = "github.com/jzelinskie/faq";
